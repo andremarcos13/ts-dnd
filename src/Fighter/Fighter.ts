6718d17d8f6b3e0f99928efd0c140f2b33ca12a5
Andre Marcos <andrmgc@gmail.com>
2022-10-18 17:10:12 -0300

    req 06

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,13 @@
+import Energy from '../Energy';
+
+export default interface Fighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  // https://stackoverflow.com/questions/47942141/optional-property-class-in-typescript
+  energy?: Energy;
+  attack(enemy: Fighter): void;
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
